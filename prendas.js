bf7d363e2bfe27f90888f0caf28abfe064aa0fa9
ValenCamara0 <valencamara01@gmail.com>
2024-04-18 21:43:23 -0300

    Agrego archivo array prendas

---
+++ prendas.js
@@ -0,0 +1,56 @@
+const prendas = [
+    {
+        id: 1,
+        tema: "Remeras",
+        nombre: "Remeron TopCut",
+        color: "Negro",
+        talle: "L",
+        precio: 150,
+        stock: true
+    },
+    {
+        id: 2,
+        tema: "Pantalones",
+        nombre: "Pantalo Oversize",
+        color: "Negro",
+        talle: 44,
+        precio: 250,
+        stock: true
+    },
+    {
+        id: 3,
+        tema: "Camisas",
+        nombre: "Camisa de Lino",
+        color: "Blanca",
+        talle: "L",
+        precio: 200,
+        stock: true
+    },
+    /* {
+        id: 4,
+        tema: "Camisas",
+        nombre: "Camisa T.Woods",
+        color: "Marron",
+        talle: "L",
+        precio: 250,
+        stock: true
+    }, */
+    {
+        id: 4,
+        tema: "Bermudas",
+        nombre: "Bermuda Ancho",
+        color: "Negro",
+        talle: 44,
+        precio: 100,
+        stock: true
+    },
+    {
+        id: 5,
+        tema: "Medias",
+        nombre: "Medias 3/4",
+        color: "Blancas",
+        talle: "43",
+        precio: 50,
+        stock: true
+    }
+]
